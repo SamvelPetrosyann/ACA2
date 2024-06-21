48601cc8a0b5add7ca294085f59f50169f788f02
SamvelPetrosyann <samvelpetrosyan_2@mail.ru>
2024-06-22 02:34:38 +0400

    my Vector add resize() and reserve() functions

---
+++ Vector/Vector.h
@@ -0,0 +1,32 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+#include <initializer_list>
+
+template <typename T>
+class Vector
+{
+private:
+    int m_size;
+    int m_cap;
+    T* m_arr;
+public:
+    Vector();
+    Vector(std::initializer_list<T> init);
+    ~Vector();
+    Vector(const Vector& other);
+    Vector(Vector&& other);
+    void push_back(const T& elem);
+    void push_front(const T& elem);
+    void pop_back();
+    void pop_front();
+    void resize(int n);
+    void reserve(int n);
+    void insert(const T& elem, int position);
+    Vector& operator=(const Vector& other);
+    Vector& operator=(Vector&& other);
+    void PrintVector() const;
+};
+
+#endif // VECTOR_H
---
+++ Vector/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "Vector.h"
+
+int main()
+{
+    Vector<int> v = {1, 2, 3, 4, 5};
+    v.push_front(7);
+    v.PrintVector();
+    v.pop_back();
+    std::cout << std::endl;
+    v.PrintVector();
+    v.pop_front();
+    std::cout << std::endl;
+    v.PrintVector();
+    v.insert(5,2);
+    std::cout << std::endl;
+    v.PrintVector();
+    v.resize(9);
+    std::cout << std::endl;
+    v.PrintVector();
+    v.reserve(6);
+    std::cout << std::endl;
+    v.PrintVector();
+    
+    
+    return 0;
+}
